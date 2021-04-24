c78252cd95970f635d3a197b70dce4f286d5dfa8
Raheem <raheemideadev@gmail.com>
2021-04-24 19:36:04 +0500

    explosion animation

--- app/src/main/java/xyz/teamgravity/explosionfab/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/explosionfab/MainActivity.kt
@@ -1,7 +1,11 @@
 package xyz.teamgravity.explosionfab
 
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import xyz.teamgravity.explosionfab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val animation = AnimationUtils.loadAnimation(this, R.anim.circle_scale).apply {
+            duration = 700
+            interpolator = AccelerateDecelerateInterpolator()
+        }
+
+        binding.apply {
+            explosionB.setOnClickListener {
+                explosionB.visibility = View.INVISIBLE
+                animationView.visibility = View.INVISIBLE
+
+                animationView.startAnimation(animation) {
+                    // animation end callback
+                    root.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.purple_500))
+                    animationView.visibility = View.INVISIBLE
+                }
+            }
+        }
     }
 }
